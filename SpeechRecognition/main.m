154cc4e39de3a2e93b6d89aba3026e3a301ebabd
Michael Boosalis <michaelboosalis@michaels-macbook-air-2.local>
2016-05-20 14:42:10 +0300

    Initial Commit

---
+++ SpeechRecognition/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SpeechRecognition
+//
+//  Created by Michael Boosalis on 5/20/16.
+//  Copyright © 2016 Michael Boosalis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
